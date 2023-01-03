50b0fdc0af03aede539e491646b4913114c31f43
Shtymak <vova.shtimak@gmail.com>
2023-01-03 12:58:32 +0200

    Add express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello from index.js");
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
